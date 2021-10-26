fa5b1140bd8cc277cdb252c1c5ba9e4a0cec3fc8
unknown <clayton.lustosa@gmail.com>
2021-10-26 08:26:03 -0300

    'Inicial'

---
+++ blog/local_settings.py
@@ -0,0 +1,22 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_django_db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = None
+SESSION_COOKIE_SECURE = None
+CSRF_COOKIE_SECURE = None
+
